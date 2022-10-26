d1d6de071bda894a9741b668ee5d63d538406750
anotherNickName <bwg.it236@gmail.com>
2022-10-26 18:04:15 +0300

    [+] R:

--- app/internal/repository/postgres/user_repository.py
+++ app/internal/repository/postgres/user_repository.py
@@ -40,8 +40,6 @@ class UserRepository:
                 where {where_filters}
             """
 
-            print(q % cmd.dict())
-
             await cur.execute(q, cmd.dict())
 
             resp = await cur.fetchone()
